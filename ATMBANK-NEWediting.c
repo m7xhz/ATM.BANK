967aeb05b294a10ce45d340ee2d6c376a5a4ac01
Mohamed Edbiaa <91739388+m7xhz@users.noreply.github.com>
2022-07-10 19:52:40 +0200

    Update ATMBANK-NEWediting.c

--- ATMBANK-NEWediting.c
+++ ATMBANK-NEWediting.c
@@ -1,258 +1,276 @@
 #include <stdio.h>
-#include <dos.h>
+#include <stdlib.h>
+#include <windows.h>
 
-int main()
+ int main()
 {
-  int power;
-  printf("   Welcome to Al-Aman Bank!\n\n");
-  printf("   TO START ENTER 1 OR 2\n");
-  scanf("%d", &power);
-
-  char name [50];
-  int pass;
-  printf("Enter first name: ");
-  scanf("%s", &name);
-
-  char lname [50];
-  printf("\nEnter last name: ");
-  scanf("%s", &lname);
-
-  printf("\nEnter your ID ACCOUNT: ");
-  scanf("%5d", &pass);
-  sleep (2);
-  printf("\nHello!\n%s %s\n", name, lname);
-  if (power == 1)
-  {
-    int pow = 4;
-    for (int i = 0; i < pow; i++)
+    start:
+      for(int i = 0; i <= 2; i++)
     {
-        pow++;
-        int opreator;
-        printf("  BUY:  USD TO LYD = 5.15$ | ERO TO LYD = 6.35$  | POUND TO LYD = 7.22$\n");
-        printf("  SELL: LYD TO USD = 5.30$ | LYD TO EURO = 6.45$  | LYD TO POUND = 7.39$\n");
-        printf("\n     **services**\n\nChoose the right service for you\nEnter {1 or 2}\n1.Cash Withdrawal.\n2.Currency Exchange.\n     ");
-        scanf("%d", &opreator);
+      ++i;
+      printf("\t\t\t\t\t********************************\n");
+      printf("\t\t\t\t\t* Welcome to ATM Alaman Bank! *\n");
+      printf("\t\t\t\t\t********************************\n");
+      char fname [50], lname [50];
+      printf("Enter your first name: ");
+      scanf("%s", &fname);
+      printf("Enter your last name: ");
+      scanf("%s", &lname);
+      long id;
+      printf("Enter your id account: ");
+      scanf("%lld", &id);
+      for (int i = 0; i < 6; i++)
+      {
+        printf("..");
+        sleep(1);
+      }
+      printf("\nHello! %s %s\tID ACCOUNT: %lld\n\n", fname, lname, id);
+      short int sr;
+      printf("1. Cash Withdrawal \t 2.Exchange Currency\n");
+      scanf("%d", &sr);
 
-        if (opreator == 1)
+      if (sr == 2)
+      {
+        printf("\n** USD TO LYD: 5.15$ ** || ** EURO TO LYD: 6.15$ ** || ** POUND TO LYD: 7.10$ **\n");
+        printf("** LYD TO USD: 5.30$ ** || ** LYD TO EURO: 6.35$ ** || ** LYD TO POUND: 7.25$ **\n");
+
+        printf("\n**Services**\n1.USD TO LYD\n2.EURO TO LYD\n3.POUND TO LYD\n");
+        printf("4.LYD TO USD\n5.LYD TO EURO\n6.LYD TO POUND\n");
+
+
+        short int operator;
+        printf("Choose the right service for you: ");
+        scanf("%d", &operator);
+
+        switch (operator)
         {
-          float s, tax;
-          printf("\nEnter the value: ");
-          scanf("%f", &s);
-
-          if (s > 1000)
-          {
-            tax = 2.5 * s / 100;
-            printf("The value: %.2f$\nTax: %.2f$\n", s , tax);
-            int g;
-            printf("Are you agreed?\n1.Yes.\n2.No\n");
-            scanf("%d", &g);
-            if (g == 1)
-            {
-              printf("Money is being withdrawn\nPlease wait.........\n");
-              sleep (5);
-              printf("Take your money! \n\n");
-              sleep (5);
-            }
-            else
-            {
-              printf("Operation canceled successfully\n");
-              sleep (5);
-            }
-          }
-          else
-          {
-            int gt;
-            tax = 2 * s / 100;
-            printf("The value: %.2f$\nTax: %.2f$\n", s , tax);
-            printf("Are you agreed?\n1.Yes.\n2.No\n");
-            scanf("%d", &gt);
-            if (gt == 1)
-            {
-              printf("Money is being withdrawn\nPlease wait.........\n");
-              sleep (5);
-              printf("Take your money! \n\n");
-              sleep (5);
-            }
-            else
-            {
-              printf("Operation canceled successfully\n");
-              sleep (5);
-            }
-          }
-
-        }
-
-        else if (opreator == 2)
-        {
-          int op;
-          printf("1.USD to LYD\n2.EURO to LYD\n");
-          printf("3.POUND to LYD\n4.LYD to USD\n5.LYD to EURO\n6.LYD to POUND\n");
-          scanf("%d", &op);
-
-          switch (op)
-          {
-            //** USD to LYD **
-            case 1:
-            float usd, exs, tax;
-
-            printf("Enter the value (USD): ");
-            scanf("%f", &usd);
+        case 1:
+                float usd, exs, tax, lyd, pound, euro;
+                printf("\nEnter the value (USD): \n");
+                scanf("%f", &usd);
             exs = usd * 5.15;
             tax = 1.15 * exs / 100;
-            printf("LYD: %.2f LYD\nTax: %.2f USD\n\n", exs, tax / 5.15);
-            int agree;
-            printf("Are you agreed?\n1. Yes\n2. No\n");
-            scanf("%d", &agree);
+            sleep (3);
+            printf("LYD: %.2f$\nTax: %.2f$\n", exs, tax);
+            int g;
+            printf("Are you agreed?\n1. Agreed.\n2. Not agreed\n");
+            scanf("%d", &g);
 
-            if (agree == 1)
+            if (g == 1)
             {
-              printf("Money is being withdrawn\nPlease wait.........\n\n");
-              sleep (5);
-              printf("Take your money! \n\n");
-              sleep (5);
+                printf("Please wait...");
+                for (int i = 0; i < 6; i++)
+                {
+                  printf("..");
+                  sleep(1);
+                }
+                printf("\nPlease take your money!!\n");
+                sleep (4);
+                printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n_\n");
             }
-            else if (agree == 2)
+            else if (g == 2)
             {
-              printf("Operation canceled successfully\n");
-              sleep (5);
+                printf("Operation canceled successfully\n");
+                sleep (3);
+                printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n_\n");
+            }
 
-            }
             break;
 
-            //** EURO to LYD **
-            case 2:
-            float euro;
+                case 2:
 
-            printf("Enter the value (EURO): ");
-            scanf("%f", &euro);
-            exs = euro * 6.35;
+                printf("\nEnter the value (EURO): \n");
+                scanf("%f", &euro);
+            exs = euro * 6.15;
             tax = 1.15 * exs / 100;
-            printf("LYD: %.2f LYD\nTax: %.2f EURO\n\n", exs, tax / 6.35);
+            sleep (4);
+            printf("LYD: %.2f$\nTax: %.2f$\n", exs, tax);
+                    printf("Are you agreed?\n1. Agreed.\n2. Not agreed\n");
+                    scanf("%d", &g);
 
-            printf("Are you agreed?\n1. Yes\n2. No\n");
-            scanf("%d", &agree);
-
-            if (agree == 1)
+            if (g == 1)
             {
-              printf("Money is being withdrawn\nPlease wait.........\n\n");
-              sleep (5);
-              printf("Take your money! \n\n");
-              sleep (5);
+                printf("Please wait...");
+                for (int i = 0; i < 6; i++)
+                {
+                  printf("..");
+                  sleep(1);
+                }
+                printf("\nPlease take your money!!\n");
+                sleep (4);
+                printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n_\n");
             }
-            else if (agree == 2)
+            else if (g == 2)
             {
-              printf("Operation canceled successfully\n");
-              sleep (5);
+                printf("Operation canceled successfully\n");
+                sleep (4);
+                printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n_\n");
+            }
+
+            break;
+
+                case 3:
+
+                printf("\nEnter the value (POUND): \n");
+                scanf("%f", &pound);
+            exs = pound * 7.10;
+            tax = 1.15 * exs / 100;
+            sleep (4);
+            printf("LYD: %.2f$\nTax: %.2f$\n", exs, tax);
+                    printf("Are you agreed?\n1. Agreed.\n2. Not agreed\n");
+                    scanf("%d", &g);
+
+            if (g == 1)
+            {
+                printf("Please wait....");
+                for (int i = 0; i < 6; i++)
+                {
+                  printf("..");
+                  sleep(1);
+                }
+                printf("\nPlease take your money!!\n");
+                sleep (4);
+                printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n_\n");
+            }
+            else if (g == 2)
+            {
+                printf("Operation canceled successfully\n");
+                sleep (4);
+                printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n_\n");
             }
             break;
 
-              //** POUND to LYD **
-              case 3:
-              float ponud;
-              printf("Enter the value (Ponud): ");
-              scanf("%f", &ponud);
-              exs = ponud * 7.22;
-              tax = 1.15 * exs / 100;
-              printf("LYD: %.2f LYD\nTax: %.2f POUND\n\n", exs, tax / 7.22);
-              printf("Are you agreed?\n1. Yes\n2. No\n");
-              scanf("%d", &agree);
+                case 4:
 
-              if (agree == 1)
-              {
-                printf("Money is being withdrawn\nPlease wait.........\n\n");
-                sleep (5);
-                printf("Take your money! \n\n");
-                sleep (5);
-              }
-              else if (agree == 2)
-              {
+                printf("\nEnter the value (LYD): \n");
+                scanf("%f", &lyd);
+                exs = lyd / 5.30;
+                tax = 1.15 * exs / 100;
+
+            printf("USD: %.2f$\nTax: %.2f$\n", exs, tax);
+                    printf("Are you agreed?\n1. Agreed.\n2. Not agreed\n");
+                    scanf("%d", &g);
+
+            if (g == 1)
+            {
+                printf("Please wait....");
+                for (int i = 0; i < 6; i++)
+                {
+                  printf("..");
+                  sleep(1);
+                }
+                printf("\nPlease take your money!!\n");
+                sleep (4);
+                printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n_\n");
+            }
+            else if (g == 2)
+            {
                 printf("Operation canceled successfully\n");
-                sleep (5);
-              }
-              break;
+                sleep (4);
+                printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n_\n");
+            }
+            break;
 
-              //** LYD to USD **
-              case 4:
-              float lyd;
-              printf("Enter the value (LYD): ");
-              scanf("%f", &lyd);
-              exs = lyd / 5.30;
-              tax = 1.15 * exs / 100;
-              printf("USD: %.2f$\nTax: %.2f$ USD\n\n", exs, tax);
-              printf("Are you agreed?\n1. Yes\n2. No\n");
-              scanf("%d", &agree);
+                case 5:
 
-              if (agree == 1)
-              {
-                printf("Money is being withdrawn\nPlease wait.........\n\n");
-                sleep (5);
-                printf("Take your money! \n\n");
-                sleep (5);
-              }
-              else if (agree == 2)
-              {
+                printf("\nEnter the value (LYD): \n");
+                scanf("%f", &lyd);
+            exs = lyd / 6.35;
+            tax = 1.15 * exs / 100;
+            sleep (4);
+            printf("EURO: %.2f$\nTax: %.2f$\n", exs, tax);
+                    printf("Are you agreed?\n1. Agreed.\n2. Not agreed\n");
+                    scanf("%d", &g);
+
+            if (g == 1)
+            {
+                printf("Please wait....");
+                for (int i = 0; i < 6; i++)
+                {
+                  printf("..");
+                  sleep(1);
+                }
+                printf("\nPlease take your money!!\n");
+                sleep (4);
+                printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n_\n");
+            }
+            else if (g == 2)
+            {
                 printf("Operation canceled successfully\n");
-                sleep (5);
-              }
-              break;
+                sleep (4);
+              printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n_\n");
+            }
+            break;
 
-              //** LYD to EURO **
-              case 5:
-              printf("Enter the value (LYD): ");
-              scanf("%f", &lyd);
-              exs = lyd / 6.45;
-              tax = 1.15 * exs / 100;
-              printf("EURO: %.2f$\nTax: %.2f$ EURO\n\n", exs, tax);
-              printf("Are you agreed?\n1. Yes\n2. No\n");
-              scanf("%d", &agree);
+                case 6:
 
-              if (agree == 1)
-              {
-                printf("Money is being withdrawn\nPlease wait.........\n\n");
-                sleep (5);
-                printf("Take your money! \n\n");
-                sleep (5);
-              }
-              else if (agree == 2)
-              {
+                printf("\nEnter the value (LYD): \n");
+                scanf("%f", &pound);
+            exs = pound / 7.25;
+            tax = 1.15 * exs / 100;
+            sleep (4);
+            printf("POUND: %.2f$\nTax: %.2f$\n", exs, tax);
+                    printf("Are you agreed?\n1. Agreed.\n2. Not agreed\n");
+                    scanf("%d", &g);
+
+            if (g == 1)
+            {
+                printf("Please wait....");
+                for (int i = 0; i < 6; i++)
+                {
+                  printf("..");
+                  sleep(1);
+                }
+                printf("\nPlease take your money!!\n");
+                sleep (4);
+                printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n_\n");
+            }
+            else if (g == 2)
+            {
                 printf("Operation canceled successfully\n");
-                sleep (5);
-              }
-              break;
+                sleep (4);
+                printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n_\n");
+            }
+            break;
 
-              //** LYD to POUND **
-              case 6:
-              printf("Enter the value (LYD): ");
-              scanf("%f", &lyd);
-              exs = lyd / 7.39;
-              tax = 1.15 * exs / 100;
-              printf("Pound: %.2f$\nTax: %.2f$ POUND\n\n", exs, tax);
-              printf("Are you agreed?\n1. Yes\n2. No\n");
-              scanf("%d", &agree);
-
-              if (agree == 1)
-              {
-                printf("Money is being withdrawn\nPlease wait.........\n\n");
-                sleep (5);
-                printf("Take your money! \n\n");
-                sleep (5);
-              }
-              else if (agree == 2)
-              {
-                printf("Operation canceled successfully\n");
-                sleep (5);
-              }
-              break;
-           }
-
+        default:
+        printf("ERORR!!\n");
+        goto start;
+            break;
         }
 
-    }
-  }
+      }
+      else if (sr == 1)
+      {
+          float m, tax;
+          printf("Enter the value: ");
+          scanf("%f", &m);
+          tax = 1.15 * m / 100;
+          printf("The value: %.2f$\nTax: %.2f$\n", m, tax);
+          int gr;
+          printf("Are you agreed!\n1.Agreed\t2.Not agreed\n");
+          scanf("%d", &gr);
+          if (gr == 1)
+          {
+            printf("Please wait....");
+            for (int i = 0; i < 6; i++)
+            {
+              printf("..");
+              sleep(1);
+            }
+            printf("\nPlease take your money!!\n");
+            sleep (4);
+          printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n_\n");
+          }
+          else if (gr == 2)
+          {
+            printf("canceled successfully\n");
+            sleep (4);
+            printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n_\n");
+            goto start;
+          }
+      }
 
-  else
-  {
-    printf("Operation canceled successfully\n");
-  }
-  return 0;
+    }
+
 }
